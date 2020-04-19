e6e2ab91bcaa7eda35533658c0ad7ae01b233c4e
Richie Bendall <richiebendall@gmail.com>
2020-04-20 05:13:35 +1200

    Require Node.js 10 (#12)

--- index.js
+++ index.js
@@ -1,12 +1,9 @@
 'use strict';
 const fs = require('fs');
-const {promisify} = require('util');
-
-const pAccess = promisify(fs.access);
 
 module.exports = async path => {
 	try {
-		await pAccess(path);
+		await fs.promises.access(path);
 		return true;
 	} catch (_) {
 		return false;
